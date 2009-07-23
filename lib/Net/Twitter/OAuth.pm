d6fd9ef497646f5b753385c9797d2ac5adf9b8de
Marc Mims <marc@questright.com>
2009-07-23 15:53:08 -0700

    CPAN release 3.04003

--- lib/Net/Twitter/OAuth.pm
+++ lib/Net/Twitter/OAuth.pm
@@ -2,7 +2,7 @@ package Net::Twitter::OAuth;
 use Moose;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.04002';
+our $VERSION = '3.04003';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
 extends  'Net::Twitter::Core';
