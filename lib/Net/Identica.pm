0d75eb22f565cdd477157c5ff8fed8841c9e8006
Marc Mims <marc@questright.com>
2009-08-21 13:47:03 -0700

    CPAN release 3.05001

--- lib/Net/Identica.pm
+++ lib/Net/Identica.pm
@@ -2,7 +2,7 @@ package Net::Identica;
 use Moose;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.05000';
+our $VERSION = '3.05001';
 $VERSION     = eval $VERSION; # numify for warning-free dev releases
 
 extends 'Net::Twitter::Core';
--- lib/Net/Twitter/Core.pm
+++ lib/Net/Twitter/Core.pm
@@ -14,7 +14,7 @@ use Encode;
 use namespace::autoclean;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.05000';
+our $VERSION = '3.05001';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
