930d4ef3e95f548a71ee9053060149c063212589
Marc Mims <marc@questright.com>
2010-07-06 13:42:47 -0700

    CPAN release 3.13007

--- lib/Net/Twitter.pm
+++ lib/Net/Twitter.pm
@@ -11,7 +11,7 @@ has '_trait_namespace' => (
 );
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.13006';
+our $VERSION = '3.13007';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
--- lib/Net/Twitter/Core.pm
+++ lib/Net/Twitter/Core.pm
@@ -17,7 +17,7 @@ use Data::Visitor::Callback;
 use namespace::autoclean;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.13006';
+our $VERSION = '3.13007';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
