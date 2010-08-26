0e9410928d60ba98840cc868f3eb42059faa13e9
Marc Mims <marc@questright.com>
2010-08-26 10:25:38 -0700

    CPAN release 3.13008

--- lib/Net/Twitter.pm
+++ lib/Net/Twitter.pm
@@ -11,7 +11,7 @@ has '_trait_namespace' => (
 );
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.13007';
+our $VERSION = '3.13008';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
