fa663772f1980f14cb96fe299cd96668a5533638
Marc Mims <marc@questright.com>
2010-11-02 10:20:53 -0700

    Fixed: use File::Spec tests that failed in Win32 (turugina)

--- lib/Net/Identica.pm
+++ lib/Net/Identica.pm
@@ -2,7 +2,7 @@ package Net::Identica;
 use Moose;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.14001';
+our $VERSION = '3.14002';
 $VERSION     = eval $VERSION; # numify for warning-free dev releases
 
 extends 'Net::Twitter::Core';
--- lib/Net/Twitter.pm
+++ lib/Net/Twitter.pm
@@ -11,7 +11,7 @@ has '_trait_namespace' => (
 );
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.14001';
+our $VERSION = '3.14002';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
