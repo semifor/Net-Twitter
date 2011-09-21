5d6c92fa9ed4d3e7bbfc1ce642b7c28d01f49a56
Marc Mims <marc@questright.com>
2011-09-21 15:22:31 -0700

    CPAN developer release 3.18000_01

--- lib/Net/Twitter/Search.pm
+++ lib/Net/Twitter/Search.pm
@@ -2,7 +2,7 @@ package Net::Twitter::Search;
 use Moose;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.17001';
+our $VERSION = '3.18000_00';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
 extends 'Net::Twitter::Core';
