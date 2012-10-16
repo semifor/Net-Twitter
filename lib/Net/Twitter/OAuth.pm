ee341ca85a56a5834464b458999fccfa72d52450
Marc Mims <marc@questright.com>
2012-10-15 17:56:22 -0700

    Release 3.18004

--- lib/Net/Twitter/OAuth.pm
+++ lib/Net/Twitter/OAuth.pm
@@ -2,7 +2,7 @@ package Net::Twitter::OAuth;
 use Moose;
 
 # use *all* digits for fBSD ports
-our $VERSION = '3.18003';
+our $VERSION = '3.18004';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
 extends  'Net::Twitter::Core';
